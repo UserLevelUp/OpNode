fe769970b0a09f842078420587f2732b18c4f23c
mnoon <mnoon@a30629b2-8912-0410-8c62-8da790ff8094>
2010-12-25 07:51:17 +0000

    Added abstract class to do all the wierd serialization etc...

---
+++ pWord4/pWordLib/mgr/Operator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using pWordLib.dat;
+using System.Drawing;
+using myPword.dat;
+using System.Diagnostics;
+
+namespace pWordLib.mgr
+{
+    [Serializable()]
+    public abstract class Operator : IOperate
+    {
+
+        public Operator()
+        {
+            //Symbol = new Icon(Icon, 
+        }
+
+        public Operator(Icon symbol)
+        {
+            this.symbol = symbol;
+        }
+
+        #region IOperate Members
+
+        abstract public pNode Operate(pNode _pNode);
+
+        private Icon symbol;
+        public  Icon Symbol
+        {
+            get { return symbol; }
+        }
+
+        #endregion
+
+    }
+}
