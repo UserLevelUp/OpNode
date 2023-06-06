11ae8d1c646fdd05b8b92d3e4e1701460d8e17be
Marc Noon <marcnoon@userlevelup.com>
2023-06-05 19:26:04 -0700

    remove un-needed namespaces

--- pWord4/pword/pWord.cs
+++ pWord4/pword/pWord.cs
@@ -15,8 +15,6 @@ using System.Net;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
-using System.Text.Encodings.Web;
-using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using System.Xml;
