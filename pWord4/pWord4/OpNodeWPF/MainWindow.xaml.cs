4876681b819034c0502a2af534d73907402bb71b
Marc Noon <marcnoon@userlevelup.com>
2023-07-08 07:36:05 -0700

    Start on new WPF TreeVeiw project with basic dockpanels and usign .net 8

---
+++ pWord4/pWord4/OpNodeWPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OpNodeWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Top = 0;
+            this.Left = SystemParameters.PrimaryScreenWidth - this.Width;
+            this.Height = SystemParameters.PrimaryScreenHeight;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = new TreeViewItem();
+            item.Header = "New Item";
+            OpNode.Items.Add(item);
+        }
+    }
+}
