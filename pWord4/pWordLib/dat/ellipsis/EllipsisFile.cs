78e9890c270b965121a87bc1043e37e3c86a7253
Marc Noon <marcnoon@msn.com>
2023-09-30 09:35:34 -0700

    update

---
+++ pWord4/pWordLib/dat/ellipsis/EllipsisFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pWordLib.dat.ellipsis
+{
+    public struct EllipsisFile
+    {
+        public string FileName { get; set; }
+        public Type FileType { get; set; }
+        public byte[] FileBuf { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
